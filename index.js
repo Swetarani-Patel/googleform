7016df63894f63ed7aa8a0ba18c8f58a2bedc91a
Swetarani Patel <sweta@Swetaranis-MacBook-Air.local>
2023-09-02 14:24:13 +0530

    remove get-ip route

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ import express from "express";
 import { Connection } from "./database/db.js";
 import cors from "cors";
 import { formsRouter } from "./routes/forms.js";
-import axios from "axios";
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/get-ip', async (req, res) => {
-  const response = await axios.get('https://api64.ipify.org?format=json');
-    const externalIP = response.data.ip;
-    res.send(`External IP address: ${externalIP}`);
-});
 
 app.use("/api/forms", formsRouter);
 
