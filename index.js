7b8f6966d484c4be47513e8c9558c1211984dd8d
Swetarani Patel <sweta@Swetaranis-MacBook-Air.local>
2023-09-02 14:18:30 +0530

    get ip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Connection } from "./database/db.js";
 import cors from "cors";
 import { formsRouter } from "./routes/forms.js";
+import axios from "axios";
 
 const app = express();
 app.use(cors());
@@ -9,10 +10,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/get-ip', async (req, res) => {
+  const response = await axios.get('https://api64.ipify.org?format=json');
+    const externalIP = response.data.ip;
+    res.send(`External IP address: ${externalIP}`);
+});
+
 app.use("/api/forms", formsRouter);
 
 const PORT = 8000;
 Connection();
+
 app.listen(PORT, () => {
   console.log(`Server is successfully running on PORT ${PORT}`);
 });
